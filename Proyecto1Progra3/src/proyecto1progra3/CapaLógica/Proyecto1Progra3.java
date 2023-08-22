b2aa81a0a16202509f8ab38b79682ce44d37c012
Franco <Franco@LAPTOP-PBI5H7L8>
2023-08-22 07:08:25 -0600

    Commint

--- Proyecto1Progra3/src/proyecto1progra3/CapaLógica/Proyecto1Progra3.java
+++ Proyecto1Progra3/src/proyecto1progra3/CapaLógica/Proyecto1Progra3.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyecto1progra3;
+package proyecto1progra3.CapaLógica;
 
 /**
  *
